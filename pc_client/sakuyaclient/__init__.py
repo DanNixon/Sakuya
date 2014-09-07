9f0f2c9b688eb1aad68d076f16a68c045a5e6cf2
Dan Nixon <dan@dan-nixon.com>
2014-09-07 19:51:51 +0100

    Added command like parsing

--- pc_client/sakuyaclient/__init__.py
+++ pc_client/sakuyaclient/__init__.py
@@ -7,24 +7,88 @@ from notification_centre import NotificationCentre
 from console_sink import ConsoleSink
 
 def run():
-    sys.stdout.write('Help me Eirin!\n')
-    console_test()
+    """
+    Gets command line options and starts Sakuya client.
+    """
+    parser = argparse.ArgumentParser(description='Client for Sakuya development aid')
 
-def console_test():
-    trac_cache_file = 'ticket_cache.txt'
-    builds_cache_file = 'builds_cache.txt'
-    builds = ['develop_clean', 'develop_incremental', 'pylint_develop']
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Increases console verbosity'
+    )
 
-    jenkins = JenkinsClient('http://builds.mantidproject.org', builds_cache_file, builds)
-    trac = TracClient('http://trac.mantidproject.org/mantid', trac_cache_file)
-    trac.set_subscriptions(['Dan Nixon'])
+    parser.add_argument(
+        '-p', '--port',
+        action='store',
+        help='Specifies serial port to communicate with TiLDA MKe'
+    )
 
-    notifications = NotificationCentre(60)
+    parser.add_argument(
+        '-i', '--interval',
+        action='store',
+        type=float,
+        default=300,
+        help='Time in seconds between polling sources'
+    )
+
+    parser.add_argument(
+        '--builds',
+        action='store',
+        default='develop_incremental,develop_clean',
+        help='Comma separated list of build jobs to watch'
+    )
+
+    parser.add_argument(
+        '--ticket-owners',
+        action='store',
+        default='Dan Nixon',
+        help='Comma separated list of owners of tickets to watch'
+    )
+
+    parser.add_argument(
+        '--jenkins-url',
+        action='store',
+        default='http://builds.mantidproject.org',
+        help='URL for Jenkins build server'
+    )
+
+    parser.add_argument(
+        '--trac-url',
+        action='store',
+        default='http://trac.mantidproject.org/mantid',
+        help='URL for Trac ticket server'
+    )
+
+    parser.add_argument(
+        '--jenkins-cache',
+        action='store',
+        default='jenkins-cache.txt',
+        help='Cache file for Jenkins build information'
+    )
+
+    parser.add_argument(
+        '--trac-cache',
+        action='store',
+        default='trac-cache.txt',
+        help='Cache file for Trac ticket information'
+    )
+
+    props = parser.parse_args()
+
+    start_client(props)
+
+def start_client(props):
+    jenkins = JenkinsClient(props.jenkins_url, props.jenkins_cache, props.builds.split(','))
+    trac = TracClient(props.trac_url, props.trac_cache)
+    trac.set_subscriptions(props.ticket_owners.split(','))
+
+    notifications = NotificationCentre(props.interval)
 
     notifications.add_notification_source('tickets', trac)
     notifications.add_notification_source('builds', jenkins)
 
-    notification_sink = ConsoleSink()
+    notification_sink = ConsoleSink(props.verbose)
 
     notifications.add_notification_sink('console1', notification_sink)
 
--- pc_client/sakuyaclient/console_sink.py
+++ pc_client/sakuyaclient/console_sink.py
@@ -2,24 +2,53 @@ import sys
 from notification_sink import NotificationSink
 
 class ConsoleSink(NotificationSink):
+    """
+    Used to print updates to stdout.
+    """
+
+    def __init__(self, verbose):
+        self._verbose = verbose
 
     def handle(self, updates):
+        """
+        Handle the new notifications.
+        """
         for source in updates.keys():
             update_type = updates[source][0]
+            source_updates = updates[source][1]
 
-            for update in updates[source][1]:
+            if len(source_updates) == 0:
+                if self._verbose:
+                    sys.stdout.write('No updates from %s\n' % source)
+                continue
+
+            for update in source_updates:
                 if update_type == 'Jenkins':
-                    if 'last_result' in update:
-                        sys.stdout.write('Build %s went from %s to %s\n' %
-                                (update['name'], update['result'], update['last_result']))
-                    else:
-                        sys.stdout.write('Build %s is %s\n' %
-                                (update['name'], update['result']))
+                    self._handle_jenkins(update)
 
                 if update_type == 'Trac':
-                    if 'last_status' in update:
-                        sys.stdout.write('Ticket %s went from %s to %s: %s\n' %
-                                (update['id'], update['status'], update['last_status'], update['summary']))
-                    else:
-                        sys.stdout.write('Ticket %s is %s: %s\n' %
-                                (update['id'], update['status'], update['summary']))
+                    self._handle_trac(update)
+
+            sys.stdout.write('----------\n')
+
+    def _handle_jenkins(self, job):
+        """
+        Handles printing a Jenkins build.
+        """
+        if 'last_result' in job:
+            sys.stdout.write('Build %s went from %s to %s\n' %
+                    (jon['name'], job['result'], job['last_result']))
+        else:
+            sys.stdout.write('Build %s is %s\n' %
+                    (job['name'], job['result']))
+
+    def _handle_trac(self, ticket):
+        """
+        Handles printing a Trac ticket.
+        """
+        if 'last_status' in ticket:
+            sys.stdout.write('Ticket %s went from %s to %s: %s\n' %
+                    (ticket['id'], ticket['status'], ticket['last_status'], ticket['summary']))
+        else:
+            sys.stdout.write('Ticket %s is %s: %s\n' %
+                    (ticket['id'], ticket['status'], ticket['summary']))
