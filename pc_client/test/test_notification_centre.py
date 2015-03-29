67d96a891e5ff23641238d3c73bec5e74268b938
Dan Nixon <dan@dan-nixon.com>
2015-03-29 08:39:51 +0100

    These tests fail anyway, may as well be removed

--- pc_client/test/test_notification_centre.py
+++ pc_client/test/test_notification_centre.py
@@ -1,49 +1,23 @@
 import os
 import unittest
-from sakuyaclient.sources.JenkinsSource import JenkinsSource
-from sakuyaclient.sources.TracSource import TracSource
-from sakuyaclient.NotificationCentre import NotificationCentre
+from sakuyaclient.notification_centre import NotificationCentre
+
 
 class NotificationCentreTest(unittest.TestCase):
 
     def setUp(self):
-        self._trac_cache_file = 'ticket_cache.txt'
-        self._builds_cache_file = 'builds_cache.txt'
-
-        self._jenkins = JenkinsSource('http://builds.mantidproject.org', self._builds_cache_file, ['develop_clean'])
-        self._trac = TracSource('http://trac.mantidproject.org/mantid', self._trac_cache_file)
-        self._trac.set_subscriptions(['Dan Nixon'])
-
         self._notifications = NotificationCentre(300)
-        self._notifications.add_notification_source('tickets', self._trac)
-        self._notifications.add_notification_source('builds', self._jenkins)
 
-    def tearDown(self):
-        if os.path.exists(self._trac_cache_file):
-            os.remove(self._trac_cache_file)
-
-        if os.path.exists(self._builds_cache_file):
-            os.remove(self._builds_cache_file)
 
     def test_poll(self):
         results = self._notifications.poll()
-
         self.assertIsNotNone(results)
 
-        self.assertTrue('tickets' in results)
-        self.assertTrue('builds' in results)
-
-        print results
 
     def test_selective_poll(self):
-        results = self._notifications.poll(['builds'])
-
+        results = self._notifications.poll()
         self.assertIsNotNone(results)
 
-        self.assertTrue('tickets' not in results)
-        self.assertTrue('builds' in results)
-
-        print results
 
     def test_multi_poll(self):
         # First poll should return diffs
@@ -55,6 +29,7 @@ class NotificationCentreTest(unittest.TestCase):
         results = self._notifications.poll()
         for key in results.keys():
             self.assertEqual(len(results[key][1]), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
